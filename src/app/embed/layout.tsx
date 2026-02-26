a608d2113ebc57d327af9f04870242597ec2bcca
Ayush Gupta <ayushguptaworks1@gmail.com>
2026-02-26 10:31:23 +0530

    Strengthen scrollbar hiding for embed iframe

--- src/app/embed/layout.tsx
+++ src/app/embed/layout.tsx
@@ -4,7 +4,7 @@ export default function EmbedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div id="embed-root">
       <style dangerouslySetInnerHTML={{
         __html: `
           html, body {
@@ -16,6 +16,15 @@ export default function EmbedLayout({
             max-height: none !important;
             margin: 0 !important;
             padding: 0 !important;
+            scrollbar-width: none !important;
+            -ms-overflow-style: none !important;
+          }
+          html::-webkit-scrollbar,
+          body::-webkit-scrollbar {
+            width: 0 !important;
+            height: 0 !important;
+            display: none !important;
+            background: transparent !important;
           }
           #__next {
             overflow: visible !important;
@@ -25,14 +34,20 @@ export default function EmbedLayout({
             width: 0px !important;
             height: 0px !important;
             display: none !important;
+            background: transparent !important;
           }
           * {
             scrollbar-width: none !important;
             -ms-overflow-style: none !important;
           }
+          *::-webkit-scrollbar {
+            width: 0 !important;
+            height: 0 !important;
+            display: none !important;
+          }
         `
       }} />
       {children}
-    </>
+    </div>
   );
 }
